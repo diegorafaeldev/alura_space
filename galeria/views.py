cb9316c4dcbb27e5022be1c8e212cf3f25d6043a
Rafael Carvalho <rafaelcarvalho@MacBook-Pro-de-Rafael.local>
2023-12-03 00:56:50 -0300

    aula 01 do curso

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+dados ={
+    1: {"nome" : "Nebulosa de Carina",
+        "legenda" : "webbtelecope.org / NASA / James Webb"},
+    2: {"nome" : "Galáxia NGC 1079",
+        "legenda" : "nasa.org / NASA / Hubble"}
+}
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html', { "cards" : dados })
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
